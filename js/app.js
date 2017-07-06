c80fa7c65d28b3026fff8079847e80b78d03774d
Lucas Gustavo <lucasgcs11@gmail.com>
2017-07-06 17:31:54 -0300

    index pronto

---
+++ js/app.js
@@ -0,0 +1,24 @@
+function openOffcanvas($element){
+	$element.addClass('open-offcanvas');
+}
+
+function hideOffcanvas($element){
+	$element.removeClass('open-offcanvas');
+}
+
+$('.open-off').on('click', function(){
+	openOffcanvas($('html'));
+})
+
+$('html,body').on('swiperight', function(){
+	openOffcanvas($('html'));
+});
+
+$('html,body').on('swipeleft', function(){
+	hideOffcanvas($('html'));
+});
+
+$('html,body').on('click', function(e){
+	if(this === e.target)
+		hideOffcanvas($('html'));
+});
